10a49acd27f52f650ebc8d7e56e6ccd4112953a0
Ori-Naftaly <youremail@domain.com>
2020-07-01 13:26:41 -0500

    Updates

---
+++ precache-manifest.30fe4ae81fb205edd2231407744f3bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0e22c53055f0a91d39b01ab71c48d4",
+    "url": "/React-Task-Manager/index.html"
+  },
+  {
+    "revision": "3388a103e294afec401d",
+    "url": "/React-Task-Manager/static/css/main.37ea1ff0.chunk.css"
+  },
+  {
+    "revision": "f4757f471cb5b1013d4a",
+    "url": "/React-Task-Manager/static/js/2.97f2a441.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Task-Manager/static/js/2.97f2a441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3388a103e294afec401d",
+    "url": "/React-Task-Manager/static/js/main.7220870e.chunk.js"
+  },
+  {
+    "revision": "4fe82ee8f2cce6f0926b",
+    "url": "/React-Task-Manager/static/js/runtime-main.65ef5766.js"
+  }
+]);
